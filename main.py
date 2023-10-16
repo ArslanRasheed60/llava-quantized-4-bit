11afa0a88214909201b0bc6b99ad57cda6c00d37
Arslan Rasheed <arslanrashee1@gmail.com>
2023-10-16 22:31:54 +0500

    update

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
 from fastapi import FastAPI, File, UploadFile
-from PIL import Image
 from io import BytesIO
-import requests
-import torch
-from transformers import AutoTokenizer
-from llava.constants import IMAGE_TOKEN_INDEX, DEFAULT_IMAGE_TOKEN, DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN
-from llava.mm_utils import tokenizer_image_token, KeywordsStoppingCriteria
-from llava.utils import disable_torch_init
-from llava.model import LlavaLlamaForCausalLM
-from llava.conversation import conv_templates, SeparatorStyle
 
-app = FastAPI()
+from transformers import AutoTokenizer, BitsAndBytesConfig
+from llava.model import LlavaLlamaForCausalLM
+import torch
 
 model_path = "4bit/llava-v1.5-13b-3GB"
 kwargs = {"device_map": "auto"}
@@ -30,14 +23,27 @@ if not vision_tower.is_loaded:
 vision_tower.to(device='cuda')
 image_processor = vision_tower.image_processor
 
-conv_mode = "llava_v0"
-conv = conv_templates[conv_mode].copy()
-roles = conv.roles
-
+import os
+import requests
+from PIL import Image
+from io import BytesIO
+from llava.conversation import conv_templates, SeparatorStyle
+from llava.utils import disable_torch_init
+from llava.constants import IMAGE_TOKEN_INDEX, DEFAULT_IMAGE_TOKEN, DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN
+from llava.mm_utils import tokenizer_image_token, get_model_name_from_path, KeywordsStoppingCriteria
+from transformers import TextStreamer
 
 def caption_image(image_file, prompt):
+    if image_file.startswith('http') or image_file.startswith('https'):
+        response = requests.get(image_file)
+        image = Image.open(BytesIO(response.content)).convert('RGB')
+    else:
+        image = Image.open(image_file).convert('RGB')
     disable_torch_init()
-    image_tensor = image_processor.preprocess(image_file, return_tensors='pt')['pixel_values'].half().cuda()
+    conv_mode = "llava_v0"
+    conv = conv_templates[conv_mode].copy()
+    roles = conv.roles
+    image_tensor = image_processor.preprocess(image, return_tensors='pt')['pixel_values'].half().cuda()
     inp = f"{roles[0]}: {prompt}"
     inp = DEFAULT_IM_START_TOKEN + DEFAULT_IMAGE_TOKEN + DEFAULT_IM_END_TOKEN + '\n' + inp
     conv.append_message(conv.roles[0], inp)
@@ -48,13 +54,17 @@ def caption_image(image_file, prompt):
     keywords = [stop_str]
     stopping_criteria = KeywordsStoppingCriteria(keywords, tokenizer, input_ids)
     with torch.inference_mode():
-        output_ids = model.generate(input_ids, images=image_tensor, do_sample=True, temperature=0.2,
-                                     max_new_tokens=1024, use_cache=True, stopping_criteria=[stopping_criteria])
+      output_ids = model.generate(input_ids, images=image_tensor, do_sample=True, temperature=0.2,
+                                  max_new_tokens=1024, use_cache=True, stopping_criteria=[stopping_criteria])
     outputs = tokenizer.decode(output_ids[0, input_ids.shape[1]:]).strip()
     conv.messages[-1][-1] = outputs
     output = outputs.rsplit('</s>', 1)[0]
-    return output
+    return image, output
 
+from fastapi import FastAPI
+from typing import Dict
+
+app = FastAPI()
 
 @app.post("/generate")
 async def generate_caption(file: UploadFile = File(...), prompt: str = ""):
@@ -62,3 +72,4 @@ async def generate_caption(file: UploadFile = File(...), prompt: str = ""):
     image = Image.open(BytesIO(image_bytes)).convert('RGB')
     result = caption_image(image, prompt)
     return {"result": result}
+
